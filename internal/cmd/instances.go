a2392c4e2e4d15c40a6d584d9b3593690e35af5c
Cezar Craciunoiu <cezar@unikraft.io>
2026-05-19 11:22:29 +0300

    feat(internal): Show boot time field by default
    Signed-off-by: Cezar Craciunoiu <cezar@unikraft.io>

--- internal/cmd/instances.go
+++ internal/cmd/instances.go
@@ -175,7 +175,7 @@ type Instance struct {
 
 	Timing struct {
 		Uptime   types.DurationMS `mirror:"instance.uptime_ms"`
-		BootTime types.DurationUS `mirror:"instance.boot_time_us"`
+		BootTime types.DurationUS `mirror:"instance.boot_time_us" field:",short"`
 		NetTime  types.DurationUS `mirror:"instance.net_time_us"`
 	}
 
